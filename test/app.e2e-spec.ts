66d178660af54edcf8ce7097b1280597eb88dd6a
Woosik Kim <iam.woosik.kim@gmail.com>
2022-06-28 20:59:12 +0900

    :white_check_mark: Add e2e test

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,10 +1,15 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
+import request from 'supertest';
 import { AppModule } from './../src/app.module';
+import { v4 as uuidv4 } from 'uuid';
 
-describe('AppController (e2e)', () => {
+describe('e2e test', () => {
   let app: INestApplication;
+  let reviewId: string;
+  let attachedPhotoIds: string[];
+  let userId: string;
+  let placeId: string;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
@@ -15,10 +20,82 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('/ (GET)', () => {
+  beforeAll(() => {
+    reviewId = uuidv4();
+    attachedPhotoIds = [uuidv4(), uuidv4()];
+    userId = uuidv4();
+    placeId = uuidv4();
+  });
+
+  describe('POST /events', () => {
+    it('ADD action', () => {
+      const addEvent = {
+        type: 'REVIEW',
+        action: 'ADD',
+        reviewId,
+        content: '좋아요!',
+        attachedPhotoIds,
+        userId,
+        placeId,
+      };
+
+      return request(app.getHttpServer())
+        .post('/events')
+        .send(addEvent)
+        .expect(201);
+    });
+
+    it('MOD action', () => {
+      const modEvent = {
+        type: 'REVIEW',
+        action: 'MOD',
+        reviewId,
+        content: '좋아요!',
+        attachedPhotoIds: [], // 사진 삭제
+        userId,
+        placeId,
+      };
+
+      return request(app.getHttpServer())
+        .post('/events')
+        .send(modEvent)
+        .expect(201);
+    });
+
+    it('DELETE action', () => {
+      const deleteEvent = {
+        type: 'REVIEW',
+        action: 'DELETE',
+        userId,
+        reviewId,
+      };
+
+      return request(app.getHttpServer())
+        .post('/events')
+        .send(deleteEvent)
+        .expect(201);
+    });
+  });
+
+  it('GET /users/:userId/points', () => {
     return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+      .get(`/users/${userId}/points`)
+      .expect(({ body }) => {
+        expect(body.point).toBe(0);
+      });
+  });
+
+  it('GET /users/:userId/point-logs', () => {
+    return request(app.getHttpServer())
+      .get(`/users/${userId}/point-logs`)
+      .expect(({ body }) => {
+        /**
+         * sort by id DESC
+         */
+        const [thirdLog, secondLog, firstLog] = body.logs;
+        expect(firstLog.amount).toBe(3);
+        expect(secondLog.amount).toBe(-1);
+        expect(thirdLog.amount).toBe(-2);
+      });
   });
 });
